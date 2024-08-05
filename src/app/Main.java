a0967d3c486ae3a1b2a3b541c498414bc0e1bf1a
Vitalii <vitaliti.yurchenko@gmail.com>
2024-08-05 20:06:43 +0300

    Add average number

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] numbers = {-98, 48, 38, -51, 83, 2, -3, 12, -71, 27,
-                -99, 48, 38, -51, 83, 2, -3, 12, -71, 27};
+        int[] numbers = {98, 48, -38, -51, 83, 2, 3, 12, 71, 27,
+                99, 48, 38, 51, 85, -2, 3, 12, -71, 27};
 
         //Виводимо на екран користувачу елементи масиву
-        System.out.println("Елементи масиву: " + Arrays.toString(numbers));
+        System.out.println("\nЕлементи масиву: " + Arrays.toString(numbers));
 
         int sumNegativeNumbers = 0; //змінна, яка накопичує і рахує суму від'ємних чисел
         int count = 0; //<-лічильник
@@ -31,12 +31,49 @@ public class Main {
 
         oddNumbers = 20 - count; //знаючи величину масиву і кількість парних чисел визначаємо кількість непарних
         System.out.println("Кількість парних чисел: " + count +
-                "\nКількість парних чисел: " + oddNumbers);
+                "\nКількість непарних чисел: " + oddNumbers);
 
+        int min = numbers[0];
+        int max = numbers[0];
+        int minIndex = 0;
+        int maxIndex = 0;
 
-        //TODO знайти найменше та найбільше значення в масиві
-        // та середнє арифметичне чисел після першого від'ємного числа
-        // + заповнити масив рандомними числами
+        for (int index = 0; index < numbers.length; index++){
+            if (numbers[index] < min){
+                min = numbers[index];
+                minIndex = index;
+            }
+            if (numbers[index] > max){
+                max = numbers[index];
+                maxIndex = index;
+            }
+        }
+        System.out.println("Найменший елемент: " + min + " (з індексом " + minIndex + ")" +
+                "\nНайбільший елемент: " + max + " (з індексом " + maxIndex + ")");
 
+        int firstNegativeNumber = -1; //створюємо змінну, яка буде знаходити перше від'ємне число
+        //-1 оскільки від'ємного числа може і не бути в масиві
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < 0) {
+                firstNegativeNumber = i;
+            }
+            break;
+        }
+
+            if (firstNegativeNumber != -1 && firstNegativeNumber < numbers.length - 1) {
+                //якщо перше від'ємне число існує в масиві і не знаходиться в кінці
+                int sum = 0; //змінна для накопичування суми чисел після першого від'ємного
+                int countNumbers = 0;
+                for (int index = firstNegativeNumber + 1; index < numbers.length; index++) {
+                    //проходимо масив після першого від'ємного числа
+                    sum += numbers[index];
+                    countNumbers++;
+                }
+                double avgSum = sum / countNumbers;
+                System.out.println("Середнє арифметичне після першого від'ємного числа: " + avgSum);
+        } else {
+                System.out.println("В масиві немає від'ємних чисел або від'ємне число знаходиться наприкінці масиву");
+            }
     }
 }
